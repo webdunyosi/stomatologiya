9708b4073a93f625f077ea4ef044025864e7e22e
webdunyosi <alimardontoshpulatov1978@gmail.com>
2026-04-29 15:32:24 +0500

    fix: adjust padding for improved layout consistency on Profile page

--- src/pages/patient/Profile.tsx
+++ src/pages/patient/Profile.tsx
@@ -78,7 +78,7 @@ const Profile = () => {
   ];
 
   return (
-    <div className="max-w-6xl mx-auto p-4 sm:p-6 lg:p-8 animate-in fade-in duration-500 relative">
+    <div className="max-w-6xl mx-auto p-0 sm:p-6 lg:p-8 animate-in fade-in duration-500 relative">
       
       {/* ================= TOAST NOTIFICATION UI ================= */}
       <div 
